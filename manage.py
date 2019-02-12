c53d71946557278fc576dd28fd0b274b1cfc7c56
realdip <realdip@gmail.com>
2019-02-12 17:42:02 +0200

    update pipfile by adding click lib

--- manage.py
+++ manage.py
@@ -2,3 +2,18 @@
 this file will manage main app and telegram bot
 "click" library will be used
 """
+import click
+
+
+def run_bot():
+    pass
+
+@click.command()
+@click.option('-b', '--runbot', is_flag=True, help="Run Telegram bot")
+def execute(runbot):
+    if runbot:
+        runbot()
+
+
+if __name__ == '__main__':
+    execute()
