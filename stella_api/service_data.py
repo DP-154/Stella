ddd0abece69476a8a36c3da98b251cd607d5b3eb
Olha <karpenko.olha532@gmail.com>
2019-02-25 11:17:23 +0200

    added stella_api/service_data and fixed models In model Price column price digit(7,2) and init

--- stella_api/service_data.py
+++ stella_api/service_data.py
@@ -1,33 +1,54 @@
+from collections import namedtuple
+
+import database.queries as q
+from stella_api.imageMetadata.coordinates_metadata import MetaDataFromCoordinates
+
+from database.db_connection import session_maker
+from database.models import User, FuelCompany, GasStation, Fuel, Images
 from stella_api.image_recognition import digit_to_price
-import database.add_data_query(function) as db_add_data_query
-
-def data_put_to_db(data_bot):
-    recognized_price=digit_to_price(data_bot.get('dbx_path'))
-    rec_data=map(data_bot,recognized_price)
-    db_add_data_query(rec_data)
 
 
-def map(data_bot,recognized_price) -> dict:
-    result = {
-        'user': data_bot.get('user_id'),
-        'gps_location': data_bot.get('gps_location'),
-        'gas_station':data_bot.get('gas_station'),
-        'fuel_company':data_bot.get('fuel_company'),
-        'date_of_price':data_bot.get('date_of_price'),
-        'is_recognised':True,
-        'image_link':data_bot.get('dbx_path'),
-        'fuel': recognized_price[:2],
-        'price':float(recognized_price[2:])
-    }
+def store_bot_data(tg_id, image_link, new_location):
+    md_from_coordinates = MetaDataFromCoordinates(new_location.latitude, new_location.longitude)
 
-    return result
+    company_name = md_from_coordinates.get_name()
 
-def get_all_company():
-    pass
+    address = md_from_coordinates.get_address()
+    try:
 
-def get_all_station_location():
-    pass
+        session = session_maker()
 
-def get_all_station_company():
-    pass
+        user = q.get_or_create(session, User, tg_id=tg_id)
 
+        company = q.get_or_create(session, FuelCompany, fuel_company_name=company_name)
+
+        gas_station = q.get_or_create(session, GasStation, fuel_company_id=company.id, address=address)
+
+        image = q.get_or_create(session, Images, link=image_link,
+                                is_recognized=False,
+                                user_id=user.id)
+
+        recognized_price_list = digit_to_price(image_link).split(',')
+        rec_fuel_type = recognized_price_list[0]
+        try:
+            rec_price = float(recognized_price_list[1])
+        except:
+            rec_price = None
+        is_premium = False
+
+        fuel = q.get_or_none(session, Fuel, fuel_type=rec_fuel_type, is_premium=is_premium)
+        if fuel is None:
+            return "There isn't a {} in database".format(rec_fuel_type)
+
+        if rec_price is not None:
+            recognition_result = namedtuple('rec_result', ['is_recognized', 'fuel_type', 'price'])
+            rr = recognition_result(True, rec_fuel_type, rec_price)
+
+            location_result = namedtuple('loc_result', ['gas_station', 'is_from_metadata'])
+            lr = location_result(gas_station.id, False)
+            q.update_image(session, image, rr, lr)
+        else:
+            return "price {} is not recognized".format(recognized_price_list[1])
+    except:
+        return "something wrong while storing"
+    return "Everything ok. Thank you"
