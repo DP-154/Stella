d41d00dc1a57ba53c555c2ba4d828ebc99d96401
Olha <karpenko.olha532@gmail.com>
2019-02-27 18:27:43 +0200

    service invocation moved to get_location

--- stella_api/service_data.py
+++ stella_api/service_data.py
@@ -18,22 +18,26 @@ def store_bot_data(tg_id, image_link, latitude, longitude):
     session = session_maker()
     stored_data = db_store_start(session, tg_id, image_link, company_name, address)
 
-    recognized_price_list = digit_to_price(image_link).split(',')
-    rec_fuel_type = recognized_price_list[0]
-    is_premium = TMP_IS_PREMIUM
-    fuel = db_get_fuel(session, rec_fuel_type, is_premium)
-    if fuel is None:
-        return "There isn't a fuel {} in database".format(rec_fuel_type)
+    recognized_info = digit_to_price(image_link).split(',')
+    is_recognized=recognized_info[0]
+    if is_recognized:
+        rec_fuel_type = recognized_info[1]
+        is_premium = TMP_IS_PREMIUM
+        fuel = db_get_fuel(session, rec_fuel_type, is_premium)
+        if fuel is None:
+            return "There isn't a fuel {} in database".format(rec_fuel_type)
 
-    if recognized_price_list[1].replace('.', '', 1).isdigit():
-        rec_price = float(recognized_price_list[1])
-        recognition_result = namedtuple('rec_result', ['is_recognized', 'fuel_type', 'price'])
-        rr = recognition_result(TMP_IS_RECOGNIZED, rec_fuel_type, rec_price)
+        if recognized_info[2].replace('.', '', 1).isdigit():
+            rec_price = float(recognized_info[2])
+            recognition_result = namedtuple('rec_result', ['is_recognized', 'fuel_type', 'price'])
+            rr = recognition_result(is_recognized, rec_fuel_type, rec_price)
 
-        location_result = namedtuple('loc_result', ['gas_station', 'is_from_metadata'])
-        lr = location_result(stored_data['gas_station'].id, TMP_IS_FROM_METADATA)
-        db_store_recognized(session, stored_data['image'], rr, lr)
+            location_result = namedtuple('loc_result', ['gas_station', 'is_from_metadata'])
+            lr = location_result(stored_data['gas_station'].id, TMP_IS_FROM_METADATA)
+            db_store_recognized(session, stored_data['image'], rr, lr)
+        else:
+            return "{} is not a float number".format(recognized_info[2])
     else:
-        return "price {} is not recognized".format(recognized_price_list[1])
+        return 'photo is not recognized'
     session.close()
     return 'Ok'
