b4262ccbfe7119da94b91e829e621a1a13ff1dab
kerch007 <5445362@ukr.net>
2019-03-14 13:42:46 +0200

    Add gas_price_detection file, update model file

---
+++ stella_api/gas_price_detection.py
@@ -0,0 +1,161 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+from skimage import io
+
+model = load_model('my_model.h5')
+# https://github.com/keras-team/keras/issues/6462#issuecomment-319232504
+model._make_predict_function()
+
+
+def get_digits(contours, hierarchy):
+    hierarchy = hierarchy[0]
+    bounding_rectangles = [cv2.boundingRect(ctr) for ctr in contours]
+    final_bounding_rectangles = []
+
+    u, indices = np.unique(hierarchy[:, -1], return_inverse=True)
+    most_common_heirarchy = u[np.argmax(np.bincount(indices))]
+
+    for r, hr in zip(bounding_rectangles, hierarchy):
+        x, y, w, h = r
+
+        if ((w * h) > 1400) and (10 <= w <= 330) and (10 <= h <= 280) and hr[3] == most_common_heirarchy:
+            final_bounding_rectangles.append(r)
+
+    return final_bounding_rectangles
+
+
+def get_roi(result):
+    kernel = np.ones((5, 5), np.uint8)
+    ret, thresh = cv2.threshold(result, 127, 255, 0)
+    thresh = cv2.erode(thresh, kernel, iterations=1)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    digits_rectangles = get_digits(contours, hierarchy)
+    return (digits_rectangles)
+
+
+def crop_images(img, roi):
+    y = roi[1]
+    x = roi[0]
+    h = roi[1] + roi[3]
+    w = roi[0] + roi[2]
+    crop_img = img[y:h, x:w]
+    return crop_img
+
+
+def digit_recognition(crop_image, roi):
+    img = cv2.resize(crop_image, (28, 28))
+    img = img[np.newaxis]
+    img = img.reshape(img.shape[0], 28, 28, 1)
+    return (np.argmax(model.predict(img)))
+
+
+class BrsmDetect:
+
+    def __init__(self, img_path):
+        self.img_path = img_path
+
+    def preproces_image(self):
+        image = io.imread(self)
+        image = cv2.resize(image, (1000, 1000))
+        y = 0
+        h = image.shape[0]
+        w = image.shape[1]
+        x = int(w * 0.45)
+        crop = image[y:y + h, x:x + w]
+        img_grey = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+        (thresh, im_bw) = cv2.threshold(img_grey, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        thresh = 127
+        ret, cropped_threshold = cv2.threshold(im_bw, 10, 255, cv2.THRESH_BINARY_INV)
+        kernel = np.ones((5, 5), np.uint8)
+        erosion = cv2.erode(cropped_threshold, kernel, iterations=1)
+        closing = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel)
+        return closing
+
+    def digit_to_price(self):
+        result = BrsmDetect.preproces_image(self)
+        roi = get_roi(result)
+        roi2 = sorted(roi, key=lambda x: int(x[1]))
+        roi_sort = [roi2[i:i + 4] for i in range(0, len(roi2), 4)]
+        for i in range(len(roi_sort)):
+            roi_sort[i] = sorted(roi_sort[i], key=lambda x: int(x[0]))
+
+        if len(roi) == 28:
+            digit = []
+            for list in roi_sort:
+                for number in list:
+                    img = BrsmDetect.preproces_image(self)
+                    crop_image1 = crop_images(img, number)
+                    digit.append(digit_recognition(crop_image1, number))
+
+                price = [str(digit[i]) + str(digit[i + 1]) for i in range(0, len(digit), 2)]
+
+            return (True, ('95E+', price[0] + '.' + price[1]), ('95E', price[2] + '.' + price[3]),
+                    ('95E Premium', price[4] + '.' + price[5]), ('92', price[6] + '.' + price[7]),
+                    ('ДП Euro', price[8] + '.' + price[9]), ('ДП', price[10] + '.' + price[11]),
+                    ('ГАЗ', price[12] + '.' + price[13]))
+
+        else:
+            return False
+
+
+class YukonDetect:
+
+    def __init__(self, img_path):
+        self.img_path = img_path
+
+    def preproces_image(image):
+
+        image = cv2.resize(image, (1000, 1000))
+        y = 0
+        h = image.shape[0]
+        w = image.shape[1]
+        x = int(w * 0.3)
+        blur = 3
+        crop = image[y:y + h, x:x + w]
+        img_grey = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+        (thresh, im_bw) = cv2.threshold(img_grey, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        thresh = 127
+        im_bw = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)[1]
+        img_blurred = cv2.GaussianBlur(im_bw, (blur, blur), 0)
+        ret, cropped_threshold = cv2.threshold(img_blurred, 10, 255, cv2.THRESH_BINARY_INV)
+        kernel = np.ones((5, 5), np.uint8)
+        erosion = cv2.erode(cropped_threshold, kernel, iterations=1)
+
+        closing = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel)
+        opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
+        result = cv2.erode(opening, kernel, iterations=1)
+        return result
+
+    def digit_to_price(self):
+        image = io.imread(self)
+        result = YukonDetect.preproces_image(image)
+        roi = get_roi(result)
+        roi = sorted(roi, key=lambda x: int(x[1]))
+        roi_sort = [roi[i:i + 4] for i in range(0, len(roi), 4)]
+        for i in range(len(roi_sort)):
+            roi_sort[i] = sorted(roi_sort[i], key=lambda x: int(x[0]))
+
+        if len(roi) == 16:
+            digit = []
+
+            for list in roi_sort:
+                for number in list:
+                    img = YukonDetect.preproces_image(image)
+                    crop_image1 = crop_images(img, number)
+                    digit.append(digit_recognition(crop_image1, number))
+
+            price = [str(digit[i]) + str(digit[i + 1]) for i in range(0, len(digit), 2)]
+
+            return (True, ('95+', price[0] + '.' + price[1]), ('95', price[2] + '.' + price[3]),
+                    ('92', price[4] + '.' + price[5]), ('ДТ', price[6] + '.' + price[7]))
+
+        else:
+            return (False)
+
+if __name__ == '__main__':
+    img_path = '/home/kerch007/data_gas/yuk3.png'
+    price = YukonDetect.digit_to_price(img_path)
+    #price = BrsmDetect.digit_to_price(img_path)
+
+    print(price)
