6ed72f7231b4293335b3b86fe6fc3a3f05decefb
Andrew Golovko <a_golovko@i.ua>
2019-03-20 14:23:38 +0200

    Merged with DP-154 dev upstream

--- flask_api/restful.py
+++ flask_api/restful.py
@@ -1,5 +1,5 @@
 import json
-from flask import request, Blueprint
+from flask import request, Blueprint, make_response
 from database.db_query_bot import (query_by_station_min_price, query_by_station_current_date, query_avg_all_stations)
 from flask_api import helpers
 from stella_api.imageMetadata.coordinates_metadata import MetaDataFromCoordinates
@@ -16,19 +16,22 @@ session = session_maker()
 @restful.route('/min_by_fuel', methods=['GET'])
 @login_required
 def min_price():
-    if request.is_json:
+    if request.headers['Content-Type'] == 'application/json':
         request_data = request.get_json()
     fuel_type = request_data["fuel_type"]
     date_of_price = request_data["date_of_price"]
     result = query_by_station_min_price(session, fuel_type, date_of_price)
     result_dict = helpers.query_to_dict(result)
-    return json.dumps(result_dict, default=helpers.to_serializable, ensure_ascii=False)
+    json_data = json.dumps(result_dict, default=helpers.to_serializable, ensure_ascii=False)
+    resp = make_response(json_data)
+    resp.mimetype = 'application/json'
+    return resp
 
 
 @restful.route('/price_by_day', methods=['GET'])
 @login_required
 def price_by_day():
-    if request.is_json:
+    if request.headers['Content-Type'] == 'application/json':
         request_data = request.get_json()
     long = request_data["longitude"]
     lat = request_data["latitude"]
@@ -38,25 +41,34 @@ def price_by_day():
     company_address = company.get_address()
     result = query_by_station_current_date(session, company_name, company_address, date_of_price)
     result_dict = helpers.query_to_dict(result)
-    return json.dumps(result_dict, default=helpers.to_serializable, ensure_ascii=False)
+    json_data = json.dumps(result_dict, default=helpers.to_serializable, ensure_ascii=False)
+    resp = make_response(json_data)
+    resp.mimetype = 'application/json'
+    return resp
 
 
 @restful.route('/avg_price', methods=['GET'])
 @login_required
 def avg_price():
-    if request.is_json:
+    if request.headers['Content-Type'] == 'application/json':
         request_data = request.get_json()
     date_of_price = request_data["date_of_price"]
     result = query_avg_all_stations(session, date_of_price)
     result_dict = helpers.query_to_dict(result)
-    return json.dumps(result_dict, default=helpers.to_serializable, ensure_ascii=False)
+    json_data = json.dumps(result_dict, default=helpers.to_serializable, ensure_ascii=False)
+    resp = make_response(json_data)
+    resp.mimetype = 'application/json'
+    return resp
 
 
 @restful.route('/upload_image', methods=['POST'])
 @login_required
 def upload_image():
-    if request.is_json:
-        request_data = request.get_json()
+    if request.headers['Content-Type'] in ['image/jpeg', 'image/png', 'image/tiff']:
+        request.files
+
+    else:
+        return response()
     file_id = request_data["file_id"]
     dbx_path = upload_image_to_dbx(file_id)
     return json.dumps({"dropbox_path": dbx_path})
