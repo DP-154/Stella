b900902d448e4506f04a8c359009b28584f5fdb4
kooskoo <kos.kochetkov@gmail.com>
2019-03-22 14:52:36 +0200

    Updated UI (updated image upload form)

--- stella_api/app.py
+++ stella_api/app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from stella_api.restful import restful
+# from stella_api.restful import restful
 from stella_api.auth import auth
 from stella_api.ui import ui
 from flask_login import LoginManager
@@ -10,7 +10,7 @@ from database.models import User
 
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_SECRET_KEY']
-app.register_blueprint(restful)
+# app.register_blueprint(restful)
 app.register_blueprint(auth)
 app.register_blueprint(ui)
 
--- stella_api/ui.py
+++ stella_api/ui.py
@@ -1,5 +1,4 @@
 import os
-from pprint import pprint
 
 from flask import Blueprint, render_template, redirect, url_for, flash
 from stella_api.forms import SendPhotoForm
@@ -8,8 +7,9 @@ from werkzeug.utils import secure_filename
 from flask_login import login_required
 
 from database.queries import session_scope
-from database.db_connection import engine, session_maker
 from database.db_query_bot import query_all_price_period
+from database.queries import list_fuel_company_names
+
 
 from stella_api.helpers import query_to_dict
 
@@ -18,7 +18,7 @@ ui = Blueprint('ui', __name__, url_prefix='/')
 
 
 @ui.route('/prices', methods=['POST', 'GET'])
-@login_required
+# @login_required
 def prices():
     price_list = [
         {
@@ -73,35 +73,27 @@ def prices():
         },
 
     ]
-    
+
     with session_scope() as session:
-        price_list = query_to_dict(query_all_price_period(session))
-        price_list = list(dict(price_list).values())
-        date_to_str = lambda d: f'{d.month}/{d.day}/{d.year}'
-        attr_getter = lambda x: (x['fuel_company_name'], date_to_str(x['date_of_price']))
-        companies_and_dates = map(attr_getter, price_list)
-        d = {k: [] for k in dict.fromkeys(companies_and_dates)}
-        fields = ['date_of_price', 'fuel_company_name', 'fuel_type', 'price']
-        for record in price_list:
-            date, company, fuel_type, price = [record[f] for f in fields]
-            date = date_to_str(date)
-            d[(company, date)].append((fuel_type, price))
-        price_list = [{'fuel_company_name': company,
-                       'fuel': [{'date': d, 'price': p} for d, p in fuels],
-                       'date_of_price': date}
-                      for (company, date), fuels in d.items()]
-    
+        query = query_all_price_period(session)
+        price_list = query_to_dict(query)
+        companies_list = list_fuel_company_names(session)
+
     form = SendPhotoForm(meta={'csrf': False})
+
+    form.company.choices = [(item, item) for item in companies_list]
+
     if form.validate_on_submit():
         photo = form.photo.data
         transport = DropBoxDataProvider(os.environ['DROPBOX_TOKEN'])
         filename = secure_filename(photo.filename)
         transport.file_upload(photo, ('/telegram_files/' + filename))
+
         flash('Thanks for uploading photo')
 
         return redirect(url_for('ui.prices'))
 
-    return render_template('main/main.html', price_list=price_list, form=form)
+    return render_template('main/main.html', price_list=price_list.values(), form=form)
 
 
 @ui.route('/')
