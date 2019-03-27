ea72acf9f1e5b953e9c45daf81c9c427301af514
realdip <realdip@gmail.com>
2019-03-27 13:38:34 +0200

    Set data branch: - updated searching gas station by location: now you dont need send accurate gps coordinates of gas station - "more button" - you can search by wider radius - updated text of messages - deleting and editing messages to reduce trash messages Get data: - improved only one db query "get all data per 10 days" - improved perfect pagination

--- bots/telegram_bot.py
+++ bots/telegram_bot.py
@@ -3,73 +3,110 @@ import logging
 from telegram import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, \
                      InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, \
-                         CallbackQueryHandler, Handler
-from stella_api.service_data import store_bot_data, upload_image_to_dbx
-from bots.bot_services import get_station_by_location
+                         CallbackQueryHandler, RegexHandler
+from services.service_data import store_bot_data, upload_image_to_dbx, get_telegram_upload_image_paths
+from bots.bot_services import gas_station_info, pagination_output
 import bots.constants as const
-from database.db_query_bot import *
-from database.queries import session_scope
-from flask_api.helpers import query_to_list
-from pprint import pprint
+from database.db_query_bot import query_all_price_period
+from database.db_connection import session_maker
+from stella_api.helpers import query_to_list
+
+# TODO db queries
+# TODO calendar
+# TODO database connection exeption, empty base exeption, alert user about these
 
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
-PHOTO, CHOICE, SELECT_STATION, LOCATION, SET_DATA, GET_DATA, DATALOC, IN_DEV, PAGINATION = range(9)
+PHOTO, CHOICE, SELECT_STATION, LOCATION, SET_DATA, GET_DATA, DATALOC, IN_DEV, PAGINATION,\
+    STEPCHANGE = range(10)
 
 
-def start(bot, update):
-
-    bot.send_message(chat_id=update.effective_message.chat_id,
-                     text=const.start_text,
-                     reply_markup=InlineKeyboardMarkup(const.start_keyboard))
+def start(bot, update, user_data):
+    if not user_data.get('start_msg_id'):
+        msg = bot.send_message(chat_id=update.effective_message.chat_id,
+                               text=const.start_text,
+                               reply_markup=InlineKeyboardMarkup(const.start_keyboard))
+        user_data['start_msg_id'] = msg['message_id']
+    else:
+        bot.edit_message_text(chat_id=update.effective_message.chat_id,
+                              text=const.start_text,
+                              message_id=user_data['start_msg_id'],
+                              reply_markup=InlineKeyboardMarkup(const.start_keyboard))
     return CHOICE
 
 
-def start_button(bot, update):
+def start_button(bot, update, user_data):
     query = update.callback_query
+    bot.answer_callback_query(callback_query_id=update.callback_query.id)
     if query.data == 'send':
         return send_location(bot, update)
     elif query.data == 'get':
-        return getdata(bot, update)
+        return getdata(bot, update, user_data)
 
 
-def getdata(bot, update):
-    query = update.callback_query
-    location_button = KeyboardButton('Sent location', request_location=True)
-    no_matter = KeyboardButton('Doesn\'t matter')
-    bot.send_message(chat_id=query.message.chat_id,
-                     text='Please share your location to get data depending your place '
-                          'or press "Skip" if location doesn\'t matter',
-                     reply_markup=ReplyKeyboardMarkup([[location_button], [no_matter]],
-                                                      one_time_keyboard=True, resize_keyboard=True)
-                     )
+def getdata(bot, update, user_data):
+    keyboard = const.getdata_keyboard
+    bot.edit_message_text(chat_id=update.effective_message.chat_id,
+                          message_id=user_data['start_msg_id'],
+                          text=const.getdata_text,
+                          reply_markup=InlineKeyboardMarkup(keyboard))
     return GET_DATA
 
 
-def setdata(bot, update, location, user_data):
-    stations = get_station_by_location(lat=location['latitude'], lng=location['longitude'])
+def setdata(bot, update, location, user_data, more=None):
+    if not user_data.get('radius'):
+        user_data['radius'] = 50
+    if more:
+        user_data['radius'] += 600
+        bot.answer_callback_query(text=f'radius={user_data.get("radius", "min")}m',
+                                  callback_query_id=update.callback_query.id)
+        stations = gas_station_info(
+                                    lat=location['latitude'],
+                                    lng=location['longitude'],
+                                    radius=user_data['radius']
+                                    )
+        print(len(stations))
+    else:
+        stations = gas_station_info(lat=location['latitude'], lng=location['longitude'])
     if not stations:
         bot.send_message(text="There are no gas stations in your location, please try again.",
                          chat_id=update.message.chat_id)
         return send_location(bot, update)
     user_data['stations'] = stations
-    buttons = [[InlineKeyboardButton(text=st["name"]+f'\n{st["adress"]}',
+    user_data['usr_location'] = location
+    more_button = [InlineKeyboardButton(text='more...',
+                   callback_data='more')]
+    buttons = [[InlineKeyboardButton(text=st["name"]+f'\n{st["address"]}',
                                      callback_data=stations.index(st))] for st in stations]
-    bot.send_message(text="Please choose fuel company from the list: ",
-                     chat_id=update.message.chat_id,
-                     reply_markup=InlineKeyboardMarkup(buttons))
+    buttons.append(more_button)
+    msg = bot.send_message(text="Please choose gas station from the list or tap 'more': ",
+                           chat_id=update.effective_message.chat_id,
+                           reply_markup=InlineKeyboardMarkup(buttons))
+    user_data['stations_msg_id'] = msg['message_id']
     return SELECT_STATION
+
+# TODO confirmation selected station by sending its location to user
 
 
 def select_station(bot, update, user_data):
     query = update.callback_query
+    if query.data == 'more':
+        return more_stations(bot, update, user_data)
+    bot.answer_callback_query(callback_query_id=update.callback_query.id)
     user_data['gas_st'] = user_data['stations'][int(query.data)]
     bot.send_message(text='You\'ve selected "{}".\n Please send us your photo:'
                      .format(user_data['stations'][int(query.data)]['name']),
                      chat_id=query.message.chat_id)
+    user_data.pop("radius", None)
     return PHOTO
+
+
+def more_stations(bot, update, user_data):
+    bot.delete_message(chat_id=update.effective_message.chat_id,
+                       message_id=user_data['stations_msg_id'])
+    return setdata(bot, update, user_data['usr_location'], user_data, more=True)
 
 
 def send_location(bot, update):
@@ -84,54 +121,85 @@ def send_location(bot, update):
 
 def got_location(bot, update, user_data):
     chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="Thanks!", reply_markup=ReplyKeyboardRemove())
+    bot.send_message(chat_id=chat_id, text="Ok! Searching gas stations...",
+                     reply_markup=ReplyKeyboardRemove())
     loc = update.message.location
     return setdata(bot, update, loc, user_data)
 
 
-def get_data_by_location(bot, update):
-
-    bot.send_message(chat_id=update.message.chat_id, text="ok!",
-                     reply_markup=ReplyKeyboardRemove())
-    bot.send_message(chat_id=update.message.chat_id, text="Please choose:",
-                     reply_markup=InlineKeyboardMarkup(const.data_by_loc_keyboard))
-    return DATALOC
-
-
 def dataloc(bot, update, user_data):
-    msg = bot.send_message(chat_id=update.effective_message.chat_id,
-                     text="Select types of fuel:\n\nIN DEVELOPMENT",
-                     reply_markup=ReplyKeyboardRemove())
-    with session_scope() as session:
-        response = query_all_price_period(session)
+    bot.answer_callback_query(text='Filters in development...',
+                              callback_query_id=update.callback_query.id)
+    bot.edit_message_text(chat_id=update.effective_message.chat_id,
+                          message_id=user_data['start_msg_id'],
+                          text="processing...")
+    session = session_maker()
+    response = query_all_price_period(session)
     user_data['db_output'] = query_to_list(response)
     user_data['gas_per_msg'] = 5
-    user_data['output_id'] = msg['message_id']
-
-    #bot.send_message(chat_id=update.effective_message.chat_id,
-    #                 text=output[0])
-    #pprint(output)
+    user_data['position'] = 0
     return pagination(bot, update, user_data)
 
-def pagination_output(stringlist, position, step):
-    string =""
-    limit = len(stringlist)
-    if position >= limit:
-        return 'END', position
-    for i in range(step):
-        if position >= limit:
-            break
-        string += stringlist[position]
-        position += 1
-    return string, position
 
 def pagination(bot, update, user_data):
-    output = user_data['db_output']
+    keyboard = [InlineKeyboardButton('Prev', callback_data='prev'),
+                InlineKeyboardButton(f"{user_data['gas_per_msg']} per msg", callback_data='step'),
+                InlineKeyboardButton('Next', callback_data='next')]
+    exit_button = [InlineKeyboardButton('EXIT', callback_data='exit')]
+    if (user_data['position'] + user_data['gas_per_msg']) >= (len(user_data['db_output'])):
+        keyboard[2] = InlineKeyboardButton('  ', callback_data='sleep')
+    if user_data['position'] == 0:
+        keyboard[0] = InlineKeyboardButton('  ', callback_data='sleep')
+    output, user_data['position'] = pagination_output(user_data['db_output'],
+                                                      user_data['position'],
+                                                      user_data['gas_per_msg'])
+    bot.edit_message_text(text=output,
+                          chat_id=update.effective_message.chat_id,
+                          message_id=user_data['start_msg_id'],
+                          reply_markup=InlineKeyboardMarkup([keyboard, exit_button]))
+    return PAGINATION
 
-    bot.send_message(chat_id=update.effective_message.chat_id,
-                     text=output[0]+f"{update.effective_message.message_id} {update.message.message_id}")
-    bot.edit_message_text(text=output[0], chat_id=update.effective_message.chat_id,
-                     message_id=user_data['output_id'])
+
+def pagin_handler(bot, update, user_data):
+    query = update.callback_query
+    bot.answer_callback_query(callback_query_id=update.callback_query.id)
+    if query.data == 'prev':
+        user_data['position'] = (user_data['position'] - 2*user_data['gas_per_msg'])
+        return pagination(bot, update, user_data)
+    elif query.data == 'step':
+        return step_change(bot, update, user_data)
+    elif query.data == 'next':
+        return pagination(bot, update, user_data)
+    elif query.data == 'exit':
+        return start(bot, update, user_data)
+
+
+def step_change(bot, update, user_data):
+    keyboard = [[InlineKeyboardButton('1', callback_data=1),
+                InlineKeyboardButton('2', callback_data=2),
+                InlineKeyboardButton('3', callback_data=3)],
+                [InlineKeyboardButton('4', callback_data=4),
+                InlineKeyboardButton('5', callback_data=5),
+                InlineKeyboardButton('6', callback_data=6)],
+                [InlineKeyboardButton('7', callback_data=7),
+                InlineKeyboardButton('8', callback_data=8),
+                InlineKeyboardButton('9', callback_data=9)]]
+    bot.edit_message_text(text="Choose number of gas stations per page, or type it from keyboard:",
+                          chat_id=update.effective_message.chat_id,
+                          message_id=user_data['start_msg_id'],
+                          reply_markup=InlineKeyboardMarkup(keyboard))
+    return STEPCHANGE
+
+
+def step_set(bot, update, user_data):
+    query = update.callback_query
+    bot.answer_callback_query(callback_query_id=update.callback_query.id)
+    if query:
+        user_data['gas_per_msg'] = int(query.data)
+    else:
+        user_data['gas_per_msg'] = int(update.message.text)
+    user_data['position'] = 0
+    return pagination(bot, update, user_data)
 
 
 def helpme(bot, update):
@@ -142,7 +210,8 @@ def error(bot, update, err):
     logger.warning("Update {} caused error {}".format(update, err))
 
 
-def cancel(bot, update):
+def cancel(bot, update, user_data):
+    user_data.clear()
     return ConversationHandler.END
 
 
@@ -157,14 +226,17 @@ def send_file_dbx(bot, update, user_data):
         pass
     user_id = update.message.from_user.id
     station_name = user_data['gas_st']['name']
-    adress = user_data['gas_st']['adress']
+    address = user_data['gas_st']['address']
     lat, lng = user_data['gas_st']['lat'], user_data['gas_st']['lng']
-    dbx_path = upload_image_to_dbx(file_id)
-    bot.send_message(chat_id=update.message.chat_id, text="download success! "+dbx_path)
-    response = store_bot_data(telegram_id=user_id, image_link=dbx_path, company_name=station_name,
-                              address=adress, lat=lat, lng=lng)
-    bot.send_message(chat_id=update.message.chat_id, text=response)
-    return start(bot, update)
+    tg_down_path, dbx_path = get_telegram_upload_image_paths(file_id)
+
+    dbx_path, dbx_link = upload_image_to_dbx(tg_down_path, dbx_path)
+    msg = bot.send_message(chat_id=update.message.chat_id, text="download success!\nRecognition...")
+    response = store_bot_data(telegram_id=user_id, image_link=tg_down_path, image_path=dbx_path,
+                              company_name=station_name, address=address)
+    bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg['message_id'], text=response)
+    user_data.clear()
+    return start(bot, update, user_data)
 
 
 def main(poll=True):
@@ -174,25 +246,26 @@ def main(poll=True):
     disp.add_error_handler(error)
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start), ],
+        entry_points=[CommandHandler('start', start, pass_user_data=True), ],
 
         states={
-            CHOICE: [CallbackQueryHandler(start_button)],
+            CHOICE: [CallbackQueryHandler(start_button, pass_user_data=True)],
             LOCATION: [MessageHandler(Filters.location, got_location, pass_user_data=True)],
-            GET_DATA: [MessageHandler(Filters.location, get_data_by_location),
-                       MessageHandler(Filters.text, dataloc, pass_user_data=True)],
+            GET_DATA: [CallbackQueryHandler(dataloc, pass_user_data=True)],
             DATALOC: [CallbackQueryHandler(dataloc, pass_user_data=True)],
             SELECT_STATION: [CallbackQueryHandler(select_station, pass_user_data=True)],
             PHOTO: [(MessageHandler(Filters.document, send_file_dbx, pass_user_data=True)),
                     MessageHandler(Filters.photo, send_file_dbx, pass_user_data=True)],
-            PAGINATION: [MessageHandler(Filters.all, pagination, pass_user_data=True)]
+            PAGINATION: [CallbackQueryHandler(pagin_handler, pass_user_data=True)],
+            STEPCHANGE: [CallbackQueryHandler(step_set, pass_user_data=True),
+                         RegexHandler('\d{1,3}', step_set, pass_user_data=True)]
         },
 
-        fallbacks=[CommandHandler('cancel', cancel)]
+        fallbacks=[CommandHandler('cancel', cancel, pass_user_data=True)]
     )
     disp.add_handler(conv_handler)
     disp.add_handler(CommandHandler("help", helpme))
-    disp.add_handler(CommandHandler("start", start))
+    disp.add_handler(CommandHandler("start", start, pass_user_data=True))
     disp.add_error_handler(error)
 
     if poll:
@@ -208,3 +281,8 @@ def main(poll=True):
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
