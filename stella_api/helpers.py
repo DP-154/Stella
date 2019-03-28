6c7c92a5c03d637424dcb6d352b810ea6b9f5cb9
realdip <realdip@gmail.com>
2019-03-29 01:06:15 +0200

    - Updated function query_to_list, witch compiles strings to be viewed in telegram bot - fixed small errors

--- stella_api/helpers.py
+++ stella_api/helpers.py
@@ -27,11 +27,26 @@ def query_to_dict(result):
         num += 1
     return result_dict
 
+
 def query_to_list(result):
     result_list = []
+    result_dict = {}
     for row in result:
         row_dict = row._asdict()
-        string = f"{row_dict['fuel_company_name']}\n{row_dict['address']}\nA{row_dict['fuel_type']}: {row_dict['price']} uah\n" \
-            f"price on {row_dict['date_of_price'].strftime('%Y-%m-%d %H:%M:%S')}\n\n"
+        from pprint import pprint
+        pprint(row_dict)
+        key = row_dict['date_of_price'].strftime('%Y-%m-%d %H:%M')
+        if result_dict.get(key) is None:
+            result_dict[key] = {}
+        result_dict[key]['fuel_company_name'] = row_dict['fuel_company_name']
+        result_dict[key]['address'] = row_dict['address']
+        result_dict[key][row_dict['fuel_type']] = row_dict['price']
+    for key in result_dict.keys():
+        name = result_dict[key].pop('fuel_company_name')
+        address = result_dict[key].pop('address')
+        string = f"{name}\n{address}\n"
+        for fuel in result_dict[key].items():
+            string += f"{fuel[0]}: {str(fuel[1])} uah\n"
+        string += f"price on {key}\n\n"
         result_list.append(string)
     return result_list
